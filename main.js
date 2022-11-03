013dc87998e0b4795bea6c962d7cc83eda27a850
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2022-11-03 14:28:26 +0000

    initial test set up

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 /* 
 👉 Write your kata here!
+
+- array of words
+
+
+- squats for duration of song
+(setup description scenario: x person can do a squat every 8 seconds, work out how many squats the person can do in the time of the song)
+-- take in song length in seconds
+-- you need to return the total squats the person could do as a whole number. 
+-- the squat total can only include squats completed during the song duration
+-- all times passed in will be a positive whole number
+
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+let seconds = 360
+
+export function squatTotal(seconds) {
+    return seconds / 8;
+} 
--- main.test.js
+++ main.test.js
@@ -1,1 +1,10 @@
-//👉 Write your tests below here:
+import {jest} from '@jest/globals';
+
+import { squatTotal } from './main';
+
+test(`Given seconds, return correct squat count`, () => {
+    const actual = squatTotal(360);
+    const expected = 45;
+  
+    expect(actual).toBe(expected);
+  });
