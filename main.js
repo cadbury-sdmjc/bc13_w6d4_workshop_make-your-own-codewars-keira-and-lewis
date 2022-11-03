bae1c6e437995359754eb168ea81432da1517b6a
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2022-11-03 16:44:07 +0000

    added test description

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+// you have been given a special word and an array of letters e.g. ["a", "e", "r"...]. 
+//Your job is to find the special word inside of this array and return the index of the first character's position!
+
+// For example, in the array ["b", "g", "c", "a", "t", "r", "e"]
+// if the special word was 'cat' you would need to return the number 2 as this is where the word begins in the array. 
+
 export function findIndex(word, array){
 }
