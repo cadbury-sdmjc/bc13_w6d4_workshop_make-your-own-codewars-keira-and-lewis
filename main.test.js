20864139ee0f23f4791ef3cefb3b57cbcfdf2831
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2022-11-03 16:38:04 +0000

    randomised tests added

--- main.test.js
+++ main.test.js
@@ -1,13 +1,5 @@
 import { test, expect } from '@jest/globals';
-//import { squatTotal } from './main';
 import { findIndex } from './main';
-
-/*test(`Given seconds, return correct squat count`, () => {
-    const actual = squatTotal(120);
-    const expected = 45;
-  
-    expect(actual).toBe(expected);
-  });*/
 
   test(`Given night, returns 7`, () => {
     const actual = findIndex("night", ["e", "x", "p", "n", "i", "g", "a", "n", "i", "g", "h", "t", "m", "r", "w"]);
@@ -23,9 +15,37 @@ import { findIndex } from './main';
     expect(actual).toBe(expected);
   });
 
-  test(`Given a word and an array hiding that word, returns correct index of hidden word's first char`, () => {
+  test(`Given pythagoras, return 27`, () => {
     const actual = findIndex("pythagoras", ["p", "y", "p", "p", "p", "y", "t", "h", "p", "y", "t", "h", "a", "p", "y", "t", "h", "p", "y", "t", "h", "a", "g", "p", "p", "y", "t", "p", "y", "t", "h", "a", "g", "o", "r", "a", "s", "p", "y", "t"]);
     const expected = 27;
   
     expect(actual).toBe(expected);
   });
+
+  for (let i = 0; i < 10; i++) {
+    test(`Given scissors, return randomised location`, () => {
+    let word = 'scissors'
+    let wordArray = word.split("")
+    let array= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    let num = getRandomNumber(0, 10);
+    shuffle(array)
+    array.splice(num, 0, ...wordArray)
+    const expected = num;
+    const actual = findIndex(word, array)
+    expect(actual).toBe(expected);
+  })
+}
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+  }
