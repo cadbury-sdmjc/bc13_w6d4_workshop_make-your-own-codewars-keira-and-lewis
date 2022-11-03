27d1a8ff727019449b98a071a63a16034fa6661a
Sam MC <sam+github@sdmjc.net>
2022-11-03 23:20:02 +0000

    fin

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-// you have been given a special word and an array of letters e.g. ["a", "e", "r"...]. 
+// you have been given a special word and an array of letters e.g. ["a", "e", "r"...].
 //Your job is to find the special word inside of this array and return the index of the first character's position!
 
 // For example, in the array ["b", "g", "c", "a", "t", "r", "e"]
-// if the special word was 'cat' you would need to return the number 2 as this is where the word begins in the array. 
+// if the special word was 'cat' you would need to return the number 2 as this is where the word begins in the array.
 
-export function findIndex(word, array){
+export function findIndex(word, array) {
+  const wordLength = word.length;
+  let i = 0;
+  while (i <= array.length - wordLength) {
+    if (array.slice(i, i + wordLength).join("") === word) {
+      return i;
+    }
+
+    i++;
+  }
+
+  return 0;
 }
