ac771346d6ad2f7d88959ecd1ddae5e6c77f6c87
keirastanley <keiraestanley@gmail.com>
2022-11-03 16:07:37 +0000

    Hidden word function and tests written

--- main.js
+++ main.js
@@ -11,6 +11,10 @@
 -- the squat total can only include squats completed during the song duration
 -- all times passed in will be a positive whole number
 
+Diana can do a squat every 8 seconds but every 30 seconds she gets more tired. This means every 30 seconds she takes 1 second longer to complete a squat. Your job is to calculate the number of squats Diana during a given song. Your function is given the song's duration in seconds as input. 
+
+Remember:
+-- Only squats completed during the song's runtime should be counted. This means only whole numbers should be returned.(e.g. no half squats!)
 
 */
 
@@ -18,6 +22,51 @@
 
 let seconds = 360
 
-export function squatTotal(seconds) {
+/* export function squatTotal(seconds) {
     return seconds / 8;
-} 
+} */
+
+/*export function squatTotal(seconds) {
+    //Squatting once every 8 seconds until 30 seconds
+    //THEN
+        //Every 9 seconds until 60 seconds
+    //THEN 
+        //Every 10 seconds until 90 seconds
+    //etc.
+    let squatProgress = 0;
+    let squatCount = 0;
+    let squatTime = 8;
+
+    for (let i = 0; i < seconds; i++){
+        squatProgress++;
+        if (i % 30 === 0){
+            squatTime++;
+        }
+        if (squatProgress === squatTime){
+            squatCount++;
+            squatProgress = 0;
+        }
+    }
+return squatCount;
+}*/
+
+/*
+Given a word and an array as input, user must find the index of the first character of the chosen word inside the array
+*/
+let word = "night"
+let array = ["e", "x", "p", "n", "i", "g", "a", "n", "i", "g", "h", "t", "m", "r", "w"]
+
+export function findIndex(word, array){
+
+    for (let i = 0; i < array.length; i++){
+        if (array[i] === word[0]){
+            for (let j = 1; j < word.length; j++){
+                if (word[j] === array[i + j]){
+                    if (j === word.length - 1){
+                        return i;
+                    }
+                }
+            }
+        }
+    }
+}
--- main.test.js
+++ main.test.js
@@ -1,10 +1,31 @@
-import {jest} from '@jest/globals';
+import { test, expect } from '@jest/globals';
+//import { squatTotal } from './main';
+import { findIndex } from './main';
 
-import { squatTotal } from './main';
-
-test(`Given seconds, return correct squat count`, () => {
-    const actual = squatTotal(360);
+/*test(`Given seconds, return correct squat count`, () => {
+    const actual = squatTotal(120);
     const expected = 45;
+  
+    expect(actual).toBe(expected);
+  });*/
+
+  test(`Given night, returns 7`, () => {
+    const actual = findIndex("night", ["e", "x", "p", "n", "i", "g", "a", "n", "i", "g", "h", "t", "m", "r", "w"]);
+    const expected = 7;
+  
+    expect(actual).toBe(expected);
+  });
+
+  test(`Given hackathon, return 25`, () => {
+    const actual = findIndex("hackathon", ["h", "k", "x", "r", "n", "x", "i", "h", "a", "g", "a", "n", "h", "g", "h", "t", "h", "c", "h", "a", "c", "k", "t", "r", "w", "h", "a", "c", "k", "a", "t", "h", "o", "n", "p", "r", "q", "z", "p", "t"]);
+    const expected = 25;
+  
+    expect(actual).toBe(expected);
+  });
+
+  test(`Given a word and an array hiding that word, returns correct index of hidden word's first char`, () => {
+    const actual = findIndex("pythagoras", ["p", "y", "p", "p", "p", "y", "t", "h", "p", "y", "t", "h", "a", "p", "y", "t", "h", "p", "y", "t", "h", "a", "g", "p", "p", "y", "t", "p", "y", "t", "h", "a", "g", "o", "r", "a", "s", "p", "y", "t"]);
+    const expected = 27;
   
     expect(actual).toBe(expected);
   });
